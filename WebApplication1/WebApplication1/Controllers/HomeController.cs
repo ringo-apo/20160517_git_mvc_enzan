9dd19642f1b00b7944c52d756ab0df2a73c13975
APO <ringo1625@gmail.com>
2016-05-17 06:59:48 +0900

    form変更

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kekka(int hensu1,int hensu2)
+        {
+            int hensu3;
+            hensu3 = hensu1 + hensu2;
+            ViewBag.Kekka = hensu3;
+            return View();
+        }
+
+    }
+}
